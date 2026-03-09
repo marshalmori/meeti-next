2ba43a05b43896438f983219bfc776ad9da27991
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2026-03-09 10:51:33 -0300

    Cria o layout de autenticação, página de iniciar sessão e criar conta

---
+++ app/auth/create-account/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Crear Cuenta",
+};
+
+export default function RegisterPage() {
+  return (
+    <>
+      <h1>Crear Cuenta</h1>
+    </>
+  );
+}
---
+++ app/auth/login/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Iniciar Sesión",
+};
+
+export default function LoginPage() {
+  return (
+    <>
+      <h1>Iniciar Sesión</h1>
+    </>
+  );
+}
