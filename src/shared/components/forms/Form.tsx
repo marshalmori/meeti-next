c1246ee87ecaa322ee670f8ed1f7249d02d16cd8
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2026-03-16 21:39:05 -0300

    Cria um componente que recebe classes externas

--- src/shared/components/forms/Form.tsx
+++ src/shared/components/forms/Form.tsx
@@ -1,3 +1,16 @@
-export default function Form({ children }: { children: React.ReactNode }) {
-  return <form className="mt-10 space-y-3 p-5 shadow-2xl">{children}</form>;
+import { FormHTMLAttributes } from "react";
+import clsx from "clsx";
+
+type Props = FormHTMLAttributes<HTMLFormElement>;
+
+export default function Form(props: Props) {
+  const { className } = props;
+  return (
+    <form
+      {...props}
+      className={clsx("mt-10 space-y-3 p-5 shadow-2xl", className)}
+    >
+      {props.children}
+    </form>
+  );
 }
