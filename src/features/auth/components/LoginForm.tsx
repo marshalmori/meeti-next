5ce14bc53237d86603e11198f055e79ab67f8a4b
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2026-03-16 22:35:07 -0300

    Cria um componente reutilizável para inputs

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -1,25 +1,20 @@
 "use client";
 
 import Form from "@/src/shared/components/forms/Form";
+import FormInput from "@/src/shared/components/forms/FormInput";
 import FormLabel from "@/src/shared/components/forms/FormLabel";
 
 export default function LoginForm() {
   return (
     <Form>
       <FormLabel htmlFor="email">E-mail</FormLabel>
-      <input
-        type="email"
-        id="email"
-        placeholder="Ingresa tu E-mail"
-        className="border border-slate-200 w-full p-2"
-      />
+      <FormInput type="email" id="email" placeholder="Ingresa tu E-mail" />
 
       <FormLabel htmlFor="password">Password</FormLabel>
-      <input
+      <FormInput
         type="password"
         id="password"
         placeholder="Ingresa tu Password"
-        className="border border-slate-200 w-full p-2"
       />
     </Form>
   );
--- src/shared/components/forms/FormInput.tsx
+++ src/shared/components/forms/FormInput.tsx
@@ -1,3 +1,14 @@
-export default function FormInput() {
-  return <div>FormInput</div>;
+import { InputHTMLAttributes } from "react";
+import clsx from "clsx";
+
+type Props = InputHTMLAttributes<HTMLInputElement>;
+
+export default function FormInput(props: Props) {
+  const { className } = props;
+  return (
+    <input
+      {...props}
+      className={clsx("border border-slate-600 w-full p-2", className)}
+    />
+  );
 }
