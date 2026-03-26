aa58697373b861f474db39465dd952329969aedd
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2026-03-25 23:47:45 -0300

    Finaliza o BaseAuthSchema

--- src/features/auth/schemas/authSchema.ts
+++ src/features/auth/schemas/authSchema.ts
@@ -3,4 +3,17 @@ import z from "zod";
 export const BaseAuthSchema = z.object({
   name: z.string().min(1, { error: "El nombre es obligatorio" }),
   email: z.email({ error: "E-mail no es válido" }),
+  password: z
+    .string()
+    .min(8, { error: "El Password debe ser mínimo de 8 caracteres" }),
+  passwordConfirmation: z
+    .string()
+    .min(1, { error: "El Password de confirmación no puede ir vacio" }),
+});
+
+export const SignUpSchema = BaseAuthSchema.pick({
+  name: true,
+  email: true,
+  password: true,
+  passwordConfirmation: true,
 });
