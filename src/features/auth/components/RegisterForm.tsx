ff718b66900822766c78e3848984ed19438a8564
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2026-03-30 23:06:03 -0300

    Configura o modo de validação do formulário

--- src/features/auth/components/RegisterForm.tsx
+++ src/features/auth/components/RegisterForm.tsx
@@ -18,6 +18,7 @@ export default function RegisterForm() {
     formState: { errors },
   } = useForm({
     resolver: zodResolver(SignUpSchema),
+    mode: "onSubmit",
   });
 
   console.log(errors);
