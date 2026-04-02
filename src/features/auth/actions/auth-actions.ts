b064d648926b98d7131f9f9f952ea741bb3474ed
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2026-04-01 22:32:04 -0300

    Cria o Schema Community

--- src/features/auth/actions/auth-actions.ts
+++ src/features/auth/actions/auth-actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { SignUpInput, SignUpSchema } from "../schemas/authSchema";
+import { authService } from "../services/AuthService";
 
 export async function signUpAction(input: SignUpInput) {
   const data = SignUpSchema.safeParse(input);
@@ -12,5 +13,5 @@ export async function signUpAction(input: SignUpInput) {
     };
   }
 
-  console.log(data.success);
+  await authService.register(data.data);
 }
--- src/features/auth/services/AuthService.ts
+++ src/features/auth/services/AuthService.ts
@@ -1,3 +1,15 @@
-class AuthService {}
+import { SignUpInput } from "../schemas/authSchema";
+
+class AuthService {
+  async register(credentials: SignUpInput) {
+    const { name, email, password } = credentials;
+
+    // Revisar si el usuario existe
+
+    // Validación de negocio
+
+    // Manejar el registro
+  }
+}
 
 export const authService = new AuthService();
